e51a191b14b54c1fb55448d3e42903e1b045e627
Bernhard Schmidhuber <schmidhuber.bernhard@sbg.at>
2024-07-22 16:31:25 +0200

    update experience container height

--- src/pages/exp/[envType]/[manifestId].tsx
+++ src/pages/exp/[envType]/[manifestId].tsx
@@ -1,35 +1,33 @@
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 
-const DynamicExperience = dynamic(() => import('../../../components/Experience'), {
-  ssr: false,
-});
+const DynamicExperience = dynamic(
+  () => import("../../../components/Experience"),
+  {
+    ssr: false,
+  }
+);
 
 const Exp = () => {
   const router = useRouter();
   const { envType, manifestId } = router.query;
 
-  if (typeof envType !== 'string' || typeof manifestId !== 'string') {
-    return (
-      <p>Missing params</p>
-    );
+  if (typeof envType !== "string" || typeof manifestId !== "string") {
+    return <p>Missing params</p>;
   }
 
-  if (!(envType === 'prod' || envType === 'test' || envType === 'dev')) {
-    return (
-      <p>Invalid envType value</p>
-    );
+  if (!(envType === "prod" || envType === "test" || envType === "dev")) {
+    return <p>Invalid envType value</p>;
   }
 
   return (
     <>
       <h1>Experience</h1>
-      <DynamicExperience
-        manifestId={manifestId}
-        envType={envType}
-      />
+      <div style={{ height: 500 }}>
+        <DynamicExperience manifestId={manifestId} envType={envType} />
+      </div>
     </>
   );
-}
+};
 
 export default Exp;
